f8b4d14dc6ad7f73264540ffad15bfae9aca3975
riku20020210 <riku88g@gmail.com>
2023-05-30 18:49:48 +0900

    一応リアルタイムで動いたけど、なんか遅すぎる

--- tests/YOLO_sample.py
+++ tests/YOLO_sample.py
@@ -1,4 +1,17 @@
 from ultralytics import YOLO
-model = YOLO("yolov8n.pt")
+# モデルを選択
+#model = YOLO("yolov8n.pt")
+model = YOLO("yolov8x.pt")
+#model = YOLO("yolov8x-seg.pt")
 
-results = model("https://ultralytics.com/images/bus.jpg", save=True) 
+#model.to("cuda")
+
+# 検出対象ファイル指定
+#results = model("https://ultralytics.com/images/bus.jpg", save=True)
+#results = model("./images/test2.jpg", save=True)
+#results = model("./images/test2.mp4", save=True)
+
+# WEBカメラからリアルタイム検出
+results = model(0 , show=True)
+for i in enumerate(results):
+    print(i)
